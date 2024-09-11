32b10ede8e40104366f640162a55862daf3b62a9
Niraj <nirajtech1001@gmail.com>
2024-09-11 14:32:43 +0530

    final commit

--- script.js
+++ script.js
@@ -7,15 +7,4 @@ document.querySelector('form').addEventListener('submit', function(event) {
 });
 
 
-// External JavaScript for scroll-minimizing navigation bar
 
-window.addEventListener("scroll", function() {
-    var header = document.querySelector("header");
-
-    // Add 'scrolled' class when scrolled down 50px
-    if (window.scrollY > 50) {
-        header.classList.add("scrolled");
-    } else {
-        header.classList.remove("scrolled");
-    }
-});
